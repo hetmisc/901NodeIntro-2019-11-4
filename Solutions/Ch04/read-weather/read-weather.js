362ff44db723118f7dcad136b50038de77a864f3
Judy Lipinski <judy@karmoxie.com>
2019-10-10 06:40:12 -0400

    fied issue reading json file added parse

--- Solutions/Ch04/read-weather/read-weather.js
+++ Solutions/Ch04/read-weather/read-weather.js
@@ -2,17 +2,20 @@ const weather = require('./weather.json');
 console.log(weather);
 
 // Asynchronous read
-// const fs = require("fs");
-// fs.readFile('weather.json', function (err, data) {
-//    if (err) {
-//       return console.error(err);
-//    }
-//    console.log("Asynchronous read: " + data);
+const fs = require("fs");
+fs.readFile('weather.json', function (err, data) {
+   if (err) {
+      return console.error(err);
+   }
+   console.log("Asynchronous read: " + data);
 
-//    data.forEach(element => {
-//        console.log(element.day)
-//    });
-// });
+   //convert json to object
+   data = JSON.parse(data);
+   
+   data.forEach(element => {
+       console.log(element.day)
+   });
+});
 
 
 //Using promisify

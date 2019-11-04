186d8585a0f4c788b55d09585aca46e47366bdb4
Judy Lipinski <judy@karmoxie.com>
2019-11-04 15:54:48 -0500

    fixed issue with double .js

--- Demos/Ch02-fundamentals/1modules/2shapes-export-class/shapeCalculator.js
+++ Demos/Ch02-fundamentals/1modules/2shapes-export-class/shapeCalculator.js
@@ -1,4 +1,4 @@
-const Square = require('./square.js.js');
+const Square = require('./square');
 const mySquare = new Square(3);
 console.log(`The area of mySquare is
              ${mySquare.area()}`);
